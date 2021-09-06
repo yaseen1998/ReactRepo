d2f58db256270f7ef51b5f2f7edd198b938f1390
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-07 00:18:21 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { React, Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-
+// return 
+// heloo
+function Test(){
+  return <h1>welcome to hornedbeast</h1>
+}
 class  App extends Component{
   render(){
     return(
     <>
+    <Test/>
       <Header/>
       <Main/>
       <Footer/>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return (
-            <>
-                <h1>
-                    &copy: right Yaseen 2021
-                </h1>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1>&copy: right Yaseen 2021</h1>
+      </>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
