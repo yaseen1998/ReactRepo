bd0518f7fd8ca56efaa243b8b005e6602f799ddd
yaseen1998 <86142826+yaseen1998@users.noreply.github.com>
2021-09-07 00:25:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { React, Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-
+//hello
 function Test(){
   return <h1>welcome to hornedbeast</h1>
 }
